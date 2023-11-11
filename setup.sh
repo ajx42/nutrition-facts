15fa3595173200adfa1b51ce51185338b69b6bd0
Aditya Jain <cse150001001@iiti.ac.in>
2023-11-11 16:13:41 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,3 +20,18 @@ yes | curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 wget https://nodejs.org/dist/v20.9.0/node-v20.9.0-linux-x64.tar.xz 
 tar -xvf node-v20.9.0-linux-x64.tar.xz
 export PATH=$PATH:/users/ajain442/node-v20.9.0-linux-x64/bin/
+
+# install rust
+yes 1 | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+
+# get basic python utils, update venv version as needed
+yes | sudo apt-get install python3-pip
+yes | pip install --upgrade virtualenv
+
+# get clang
+sudo apt-get install clang
+
+# get silver searcher
+sudo apt-get install silversearcher-ag
+
